dea5a6c60c1b271e475101ca5f9aba7d2c7dd45f
Albert <112762352+AlbertTonoyan2004@users.noreply.github.com>
2023-12-02 15:09:25 +0300

    Update Sec.py

--- Sec.py
+++ Sec.py
@@ -8,10 +8,19 @@ import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', action='store_true', help='show changed files (slow)')
-parser.add_argument('-f', type=argparse.FileType('w'), help='outfile for timeline')
+parser.add_argument('-f', action='store_true', help='generate timeline')
+parser.add_argument('-o', action='store_true', help='show open ports')
 args = parser.parse_args()
 
-
+def run_command(command, output_file):
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+    if result.returncode == 0:
+        with open(output_file, 'w') as file:
+            file.write(result.stdout)
+    else:
+        with open(output_file, 'w') as file:
+            file.write(f"Error executing command: {command}\n")
+            file.write(f"Error message: {result.stderr}\n")
 
 def gather_system_info(output_dir):
     # Путь к файлам результата
@@ -32,15 +41,36 @@ def gather_system_info(output_dir):
     subprocess.run(["ps aux"], stdout=open(process_file, "w"), shell=True)
     subprocess.run(["systemctl list-units --type=service --all"], stdout=open(network_file, "w"), shell=True)
 
+    # Creating subdirectories for different categories
+    os.makedirs(os.path.join(output_dir, 'mta', 'postfix'))
+    os.makedirs(os.path.join(output_dir, 'mta', 'sendmail'))
+
+    # Running additional commands and saving results to files
+    run_command('systemctl --full --all', os.path.join(output_dir, 'systemctl.txt'))
+    run_command('lshw', os.path.join(output_dir, 'lshw.txt'))
+    run_command('cat /proc/cpuinfo', os.path.join(output_dir, 'cpuinfo.txt'))
+    run_command('free -h', os.path.join(output_dir, 'memory.txt'))
+    run_command('mount', os.path.join(output_dir, 'hdd.txt'))
+    run_command('df -h', os.path.join(output_dir, 'hdd.txt'))
+    run_command('df -hi', os.path.join(output_dir, 'hdd.txt'))
+    run_command('cp /etc/passwd ' + os.path.join(output_dir, 'passwd'), os.devnull)
+    run_command('cp /etc/group ' + os.path.join(output_dir, 'group'), os.devnull)
+    # ... Add more commands as needed
+
 # Создаем каталог для результатов
+# Creating a directory for results
 output_dir = f"Output_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
 os.makedirs(output_dir)
 
-# Вызываем функцию с указанием каталога
-gather_system_info(output_dir)
+# Gathering system information
+try:
+    gather_system_info(output_dir)
+except FileNotFoundError as e:
+    print(f"Error: {e}")
+    sys.exit(1)
+
 
 # Далее можно использовать содержимое output_dir по необходимости
-
 
 def get_open_ports():
     process = Popen(["netstat", "-lntup"], stdout=PIPE, stderr=DEVNULL)
@@ -53,43 +83,6 @@ def show_open_ports():
     print("{:<10} {:<20} {:<20} {:<20}".format("Proto", "Local Address", "Foreign Address", "PID/Program name"))
     for line in open_ports:
         print(line)
-
-
-
-def run_command(command, output_file):
-    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
-    if result.returncode == 0:
-        with open(output_file, 'w') as file:
-            file.write(result.stdout)
-    else:
-        with open(output_file, 'w') as file:
-            file.write(f"Error executing command: {command}\n")
-            file.write(f"Error message: {result.stderr}\n")
-
-def gather_system_info(output_dir):
-    # Creating subdirectories for different categories
-    os.makedirs(os.path.join(output_dir, 'mta', 'postfix'))
-    os.makedirs(os.path.join(output_dir, 'mta', 'sendmail'))
-
-    # Running commands and saving results to files
-    run_command('systemctl --full --all', os.path.join(output_dir, 'systemctl.txt'))
-    run_command('lshw', os.path.join(output_dir, 'lshw.txt'))
-    run_command('cat /proc/cpuinfo', os.path.join(output_dir, 'cpuinfo.txt'))
-    run_command('free -h', os.path.join(output_dir, 'memory.txt'))
-    run_command('mount', os.path.join(output_dir, 'hdd.txt'))
-    run_command('df -h', os.path.join(output_dir, 'hdd.txt'))
-    run_command('df -hi', os.path.join(output_dir, 'hdd.txt'))
-    run_command('cp /etc/passwd ' + os.path.join(output_dir, 'passwd'), os.devnull)
-    run_command('cp /etc/group ' + os.path.join(output_dir, 'group'), os.devnull)
-    # ... Add more commands as needed
-
-# Creating a directory for results
-output_dir = f"Output_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
-os.makedirs(output_dir)
-
-# Gathering system information
-gather_system_info(output_dir)
-
 
 
 def show_changed_files():
@@ -148,9 +141,24 @@ def main():
     if args.c:
         print("Showing changed files")
         show_changed_files()
-    else:
+    elif args.f:
         print("Generating timeline")
-        show_timeline()
-        show_open_ports()  # Added line to show open ports
+        try:
+            show_timeline()
+        except FileNotFoundError as e:
+            print(f"Error: {e}")
+            sys.exit(1)
+    elif args.o:
+        print("Showing open ports")
+        show_open_ports()
+    else:
+        print("Invalid arguments. Use -h for help.")
+        parser.print_help()
+        sys.exit(1)
+
 if __name__ == "__main__":
+    if not any(vars(args).values()):
+        print("No arguments provided. Use -h for help.")
+        sys.exit(1)
     main()
+
